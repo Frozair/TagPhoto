1a72c42947b5eb6c17a1a76a7120d59fcf1c523e
Rudolph Newball <rudolph.newball@gmail.com>
2013-11-05 17:10:16 -0600

    Added Drawer to left of screen. Allows to switch between gallery and camera.

--- src/com/newrdev/tagphoto/MainActivity.java
+++ src/com/newrdev/tagphoto/MainActivity.java
@@ -1,36 +1,59 @@
 package com.newrdev.tagphoto;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.app.Activity;
 import android.content.Intent;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity{
+	private String[] _drawerItems;
+	private CharSequence _actionBarTitle;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		Button b1 = (Button) findViewById(R.id.camera);
-		Button b2 = (Button) findViewById(R.id.view);
-		b1.setOnClickListener(this);
-		b2.setOnClickListener(this);
+		_drawerItems = getResources().getStringArray(R.array.drawer_items);
+		DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+		ListView drawerList = (ListView)findViewById(R.id.left_drawer);
+		
+		drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, _drawerItems));
+		drawerList.setOnItemClickListener(new DrawerItemClickListener());
 	}
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener{
 
-	@Override
-	public void onClick(View view) {
-		Intent intent = null;
-		switch(view.getId()){
-		case R.id.camera:
-			intent = new Intent(getApplicationContext(), CameraActivity.class);
-			break;
-		case R.id.view:
-			intent = new Intent(getApplicationContext(), BrowseActivity.class);
-			break;
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			selectItem(position);
 		}
-		startActivity(intent);
+		
+		private void selectItem(int position){
+			Intent intent = null;
+			if(_drawerItems[position].contains("Camera")){
+				intent = new Intent(getApplicationContext(), CameraActivity.class);
+			}else{
+				intent = new Intent(getApplicationContext(), BrowseActivity.class);
+			}
+			startActivity(intent);
+		}
+	}
+	
+	@Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+		return false;
+	}
+	
+	public void setTitle(CharSequence title){
+		_actionBarTitle = title;
+		this.getActionBar().setTitle(this._actionBarTitle);
 	}
 }
